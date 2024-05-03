c5754fc0559169013399df719fe1023948db6223
agizatullin <agizatullin@axbit.ru>
2024-05-03 19:17:12 +0300

    Исправление ошибки

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: `//kazan_techno_spa`,
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/kazan_techno_spa/' // Замените на путь, по которому ваше приложение будет доступно в браузере
+      : '/',
   transpileDependencies: true
-})
+}
+
